68ded1446c0ce514295ba7df5eecaba1699302fa
amandabeers <amanda.green413@gmail.com>
2026-07-02 02:41:29 -0400

    Remove unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-// import apiUrl from './apiConfig'
 
 function App() {
   const [count, setCount] = useState(0)
